8b90130280073bf210cae33dddc8904cf4cc9c02
Shahd Wageeh <shahdwageeh79@gmail.com>
2026-06-20 15:59:40 +0300

    commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,30 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
-}
-
-export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, {
-        serverSelectionTimeoutMS: 30000,
-      })
-      .then((mongoose) => mongoose);
-  }
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log('MongoDB connected successfully');
+    }catch(error){
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
 }
 
 export default connectDB;
