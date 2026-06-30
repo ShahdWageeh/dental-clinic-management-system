98ce2927eb9a55802feb595ac2efa0c689aa316d
Shahd Wageeh <shahdwageeh79@gmail.com>
2026-07-01 00:05:32 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -9,11 +9,43 @@ import invoiceRoutes from './routes/invoiceRoutes.js';
 import inventoryRoutes from './routes/inventoryRoutes.js';
 import notificationRoutes from './routes/notificationRoutes.js';
 import swaggerSpec from './config/swagger.js';
-import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const swaggerHtml = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>Dental Clinic Management System API</title>
+    <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui.css" />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5.32.6/swagger-ui-standalone-preset.js"></script>
+    <script>
+      window.onload = function () {
+        window.ui = SwaggerUIBundle({
+          url: '/api-docs.json',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+          ],
+          plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl
+          ],
+          layout: 'StandaloneLayout'
+        });
+      };
+    </script>
+  </body>
+</html>
+`;
 
 app.get('/', (req, res) => {
   res.json({
@@ -29,6 +61,11 @@ app.use("/api/treatments", treatmentRoutes);
 app.use("/api/invoices", invoiceRoutes);
 app.use("/api/inventory", inventoryRoutes);
 app.use("/api/notifications", notificationRoutes);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+app.get("/api-docs.json", (req, res) => {
+  res.json(swaggerSpec);
+});
+app.get(["/api-docs", "/api-docs/"], (req, res) => {
+  res.type("html").send(swaggerHtml);
+});
 
 export default app;
